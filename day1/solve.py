1ae3992d098c3cb85aa1da30c926b2aac8bb9960
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-12-01 13:51:45 +0000

    Day 1 challenges

---
+++ day1/solve.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+import math
+
+expenses = [int(line) for line in open('day1/input.txt', 'r')]
+
+for i in {2,3}:
+    combination = combinations(expenses, i)
+    answer = math.prod([combo for combo in combination if sum(combo) == 2020][0])
+    print(answer)
