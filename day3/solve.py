8f6de2a8fcdbebfd160bbf275c8ed1b2003f1456
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-12-03 09:47:57 +0000

    Day 3 solution

---
+++ day3/solve.py
@@ -0,0 +1,24 @@
+def trees(x, y, grid):
+    curX, curY, count = 0, 0, 0
+    gridWidth, gridHeight = len(grid[0]), len(grid)
+
+    while curY < gridHeight:
+        if grid[curY][curX] == '#':
+            count += 1
+        
+        curX = (curX + x) % gridWidth
+        curY += y
+
+    return count
+
+def part1(grid):
+    print(trees(3, 1, grid))
+
+def part2(grid):
+    product = trees(1, 1, grid) * trees(3, 1, grid) * trees(5, 1, grid) * trees(7, 1, grid) * trees(1, 2, grid)
+    print(product)
+
+forest = [line.strip() for line in open('day3/input.txt', 'r')]
+
+part1(forest)
+part2(forest)
