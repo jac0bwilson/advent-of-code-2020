df6dfbeb6b7d3ee6a91566109eb043678925b516
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-12-05 11:00:54 +0000

    Day 5 solutions

---
+++ day5/solve.py
@@ -0,0 +1,28 @@
+def getSeatID(seat):
+    mapping = {
+        'F': '0',
+        'B': '1',
+        'L': '0',
+        'R': '1'
+    }
+    newstr = ''
+    for char in seat:
+        newstr += mapping[char]
+    
+    return int(newstr, 2)
+
+def part1(seats):
+    print(max(map(getSeatID, seats)))
+
+def part2(seats):
+    seatIDs = list(map(getSeatID, seats))
+    sortedIDs = sorted(seatIDs)
+
+    for i in range(len(sortedIDs)):
+        if sortedIDs[i + 1] - sortedIDs[i] == 2:
+             print(sortedIDs[i + 1] - 1)
+             break
+
+seats = [line.strip() for line in open('day5/input.txt', 'r')]
+part1(seats)
+part2(seats)
