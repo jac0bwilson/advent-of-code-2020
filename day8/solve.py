a25910e1ff57b65d1436af1941e596aaf8d173d7
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-12-08 09:55:02 +0000

    Day 8 solutions

---
+++ day8/solve.py
@@ -0,0 +1,58 @@
+import copy, re
+
+def parseInstructions(instructions):
+    output = []
+    for instruction in instructions:
+        parse = re.match(r'([a-z]{3}) ([-+]\d+)', instruction)
+        op = parse.group(1)
+        num = int(parse.group(2))
+        output.append([op, num])
+    
+    return output
+
+def runProgram(instructions):
+    acc, i = 0, 0
+    visited = set()
+    
+    while i < len(instructions):
+        if i in visited:
+            return acc, False
+
+        visited.add(i)
+        
+        op = instructions[i][0]
+        num = instructions[i][1]        
+
+        if op == 'acc':
+            acc += num
+            i += 1
+        elif op == 'jmp':
+            i += num
+        else:
+            i += 1
+
+    return acc, True
+
+def part1(instructions):
+    parsed = parseInstructions(instructions)
+    print(runProgram(parsed))  
+
+def part2(instructions):
+    parsed = parseInstructions(instructions)
+
+    for i in range(len(parsed)):
+        copyList = copy.deepcopy(parsed)
+        if copyList[i][0] == 'jmp' or copyList[i][0] == 'nop':
+            if copyList[i][0] == 'jmp':
+                copyList[i][0] = 'nop'
+            else:
+                copyList[i][0] = 'jmp'
+
+            acc, terminated = runProgram(copyList)
+            if terminated:
+                print(acc)
+                return
+
+instructions = [line.strip() for line in open('day8/input.txt', 'r')]
+part1(instructions)
+part2(instructions)
