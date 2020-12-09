41ef59d98b931594a636c73c1faf19b781b24615
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-12-09 09:18:16 +0000

    Day 9 solutions

---
+++ day9/solve.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+def part1(numbers):
+    for i in range(len(numbers)):
+        twentyFive = numbers[i:i+25]
+        comb = combinations(twentyFive, 2)
+        sums = [sum(opt) for opt in comb]
+        if numbers[i + 25] not in sums:
+            print(numbers[i + 25])
+            return
+
+def part2(numbers):
+    target = 1721308972
+    for i in range(len(numbers)):
+        for j in range(i, len(numbers)):
+            if sum(numbers[i:j+1]) == target:
+                sort = sorted(numbers[i:j+1])
+                print(sort[0] + sort[-1])
+                return
+
+numbers = [int(line.strip()) for line in open('day9/input.txt', 'r')]
+part1(numbers)
+part2(numbers)
