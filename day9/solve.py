5d3f66f0400e57e399d14ebf423daf09ec95e98e
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-12-09 09:22:28 +0000

    Little neatening

--- day9/solve.py
+++ day9/solve.py
@@ -7,10 +7,9 @@ def part1(numbers):
         sums = [sum(opt) for opt in comb]
         if numbers[i + 25] not in sums:
             print(numbers[i + 25])
-            return
+            return numbers[i + 25]
 
-def part2(numbers):
-    target = 1721308972
+def part2(numbers, target):
     for i in range(len(numbers)):
         for j in range(i, len(numbers)):
             if sum(numbers[i:j+1]) == target:
@@ -19,5 +18,5 @@ def part2(numbers):
                 return
 
 numbers = [int(line.strip()) for line in open('day9/input.txt', 'r')]
-part1(numbers)
-part2(numbers)
+target = part1(numbers)
+part2(numbers, target)
