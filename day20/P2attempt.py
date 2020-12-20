2394f5ce57c97933dcdc30cf372f7bf3f5fe2b79
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-12-20 13:09:37 +0000

    Abandoned part 2 for now

--- day20/P2attempt.py
+++ day20/P2attempt.py
@@ -1,4 +1,5 @@
 import re
+from math import sqrt
 
 def getBorders(tile):
     borders = [tile[0], tile[-1]]
@@ -52,6 +53,53 @@ def part1(tiles):
 
     print(product)
 
+def rotate(tile):
+    newTile = []
+    for i in range(len(tile)):
+        newTile.append("".join([tile[j][len(tile) - 1 - i] for j in range(len(tile))]))
+
+    return newTile
+
+def assemble(tileList):
+    sides = int(sqrt(len(tileList)))
+    corners, edges = getOuter(tileList)
+    outer = corners + edges
+    inner = filter(lambda x: x[0] not in outer, tileList)
+    assembled = [[None for _ in range(sides)] for _ in range(sides)]
+
+    if len(inner) == 0:
+        toUse = filter(lambda x: x[0] in outer, tileList)
+        
+        itemCount = 0
+        for i in range(sides):
+            for j in range(sides):
+                if itemCount == 0:
+                    assembled[i][j] = toUse[1]
+                else:
+                    ...
+                
+                itemCount += 1
+
+    else:
+        ...
+
+    return
+
+def part2(tileList):
+    # create the layout
+    assembled = assemble(tileList)
+    # remove the outer edges
+    # create overall map
+    # replace sea monsters
+    seaMonster = """\
+                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   
+    """
+    # count number of '#'s
+
+    return
+
 tiles = open('day20/input.txt', 'r').read().split('\n\n')
 tileList = []
 
@@ -62,3 +110,9 @@ for tile in tiles:
     tileList.append((tileID, splitTile[1:]))
 
 part1(tileList)
+
+# part2(tileList)
+
+# print("\n".join(tileList[0][1]))
+# print("-------------------------")
+# print("\n".join(rotate(tileList[0][1])))
